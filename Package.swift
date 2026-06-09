756dca59e347b68f0b39237fe5dfb3476d5cc3fb
Philippe Weidmann <philippe.weidmann@ik.me>
2026-06-09 21:32:19 +0200

    feat: Config boiler plate

--- Package.swift
+++ Package.swift
@@ -1,32 +1,25 @@
 // swift-tools-version: 6.4
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AIToolsLLMProvider",
+    platforms: [
+        .macOS(.v27), .iOS(.v27), .visionOS(.v27), .watchOS(.v27)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AIToolsLLMProvider",
             targets: ["AIToolsLLMProvider"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AIToolsLLMProvider",
-            swiftSettings: [
-                .enableUpcomingFeature("ApproachableConcurrency"),
-            ],
+            name: "AIToolsLLMProvider"
         ),
         .testTarget(
             name: "AIToolsLLMProviderTests",
-            dependencies: ["AIToolsLLMProvider"],
-            swiftSettings: [
-                .enableUpcomingFeature("ApproachableConcurrency"),
-            ],
+            dependencies: ["AIToolsLLMProvider"]
         ),
     ],
     swiftLanguageModes: [.v6]
--- Sources/AIToolsLLMProvider/AIToolsLLMProvider.swift
+++ Sources/AIToolsLLMProvider/AIToolsLLMProvider.swift
@@ -1,5 +1,48 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-public func hello() {
-    print("Hello, world!")
+import FoundationModels
+
+public struct ApertusModel: LanguageModel {
+    public typealias Executor = ApertusModelExecutor
+
+    let modelId = "swiss-ai/Apertus-70B-Instruct-2509"
+
+    private let productId: Int
+    private let apiKey: String
+
+    public var capabilities: LanguageModelCapabilities {
+        LanguageModelCapabilities(capabilities: [
+            .toolCalling, .guidedGeneration, .reasoning
+        ])
+    }
+
+    public var executorConfiguration: Executor.Configuration {
+        Executor.Configuration(productId: productId, apiKey: apiKey)
+    }
+
+    public init(productId: Int, apiKey: String) {
+        self.productId = productId
+        self.apiKey = apiKey
+    }
+}
+
+public struct ApertusModelExecutor: LanguageModelExecutor {
+    public typealias Model = ApertusModel
+
+    public struct Configuration: Hashable, Sendable {
+        let productId: Int
+        let apiKey: String
+    }
+
+    private let configuration: Configuration
+
+    public init(configuration: Configuration) throws {
+        self.configuration = configuration
+    }
+
+    public func respond(
+        to request: LanguageModelExecutorGenerationRequest,
+        model: ApertusModel,
+        streamingInto channel: LanguageModelExecutorGenerationChannel
+    ) async throws {
+        
+    }
 }
